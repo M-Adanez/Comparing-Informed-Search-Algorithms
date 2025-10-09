7c1310cc657a704338ef90468d423b74922ce4a7
Ander Arrondo <ander.arrondo.e@opendeusto.es>
2025-10-09 08:36:07 +0200

    Weighted A* algorithm finished

--- Weighted AStar.py
+++ Weighted AStar.py
@@ -2,38 +2,55 @@
 from grid import grid
 import numpy as np
 import heapq
+import itertools
 
 class Node:
     def __init__(self, state=(0, 0), path_cost=0, parent=None):
         self.state = state
         self.path_cost = path_cost
         self.parent = parent
+        
+    def __repr__(self):
+        return str(self.state)
 
 def f(node, goal, w):
     """Weighted A* cost estimation."""
-    return  node.path_cost + w * heuristic(node, goal)
+    return  node.path_cost + w * h(node, goal)
 
-def heuristic(node, goal):
+def h(node, goal):
     """Manhattan distance function."""
     return np.abs(goal.state[0] - node.state[0]) + \
         np.abs(goal.state[1] - node.state[1])
 
-def expand(grid, node):
-    neighs = []
-    moves = []
-    for i in range(2):
-        for j in range(2):
-            moves.append((i, j))
+def apply_move(node, move):
+    """Returns a new node applying the given move."""
+    new_x = node.state[0] + move[0]
+    new_y = node.state[1] + move[1]
+    return Node(state=(new_x, new_y), path_cost=node.path_cost + 1, parent=node)
 
-    for x, y in moves:
-        if grid[y][x] != 1:
-            neighs.append((node.state[0] + y, node.state[1] + x))
+def is_legal(node):
+    """Checks if a move is valid."""
+    if (node.state[0] < 0 or node.state[0] >= len(grid[0])) or \
+        (node.state[1] < 0 or node.state[1] >= len(grid)) or \
+            grid[node.state[1]][node.state[0]] == 1:
+        return False
+    return True
+
+def expand(grid, node):
+    """Searches for adjacent nodes."""
+    neighs = []
+    moves = [(1, 0), (0, -1), (-1, 0), (0, 1)]
+
+    for move in moves:
+        new_node = apply_move(node, move)
+        if is_legal(new_node):
+            neighs.append(new_node)
     
     return neighs
 
 
 def solution(node):
-    """Reconstructs the path given a node."""
+    """Given a node, reconstructs the path."""
     path = []
     curr = node
     while curr is not None:
@@ -42,23 +59,35 @@ def solution(node):
     return list(reversed(path))
 
 def weighted_a_star(grid, init, goal, w):
+    """Performs WA* search."""
+    counter = itertools.count() # tiebreaker
     node = init
-    frontier = heapq()
-    entry_finder = {} # map of states and f() values
-    heapq.heappush(init, f(node, goal, w))
+    frontier = []
+    heapq.heappush(frontier, (f(node, goal, w), next(counter), node))
     explored = set()
 
     while frontier:
-        node = heapq.heappop()
+        node = heapq.heappop(frontier)[2]
         if node.state == goal.state:
             return solution(node)
         
         explored.add(node)
         for child in expand(grid, node):
             if child not in explored:
-                heapq.heappush(frontier, f(child, goal, w))
-            # TODO: revisar esta condicion
+                heapq.heappush(frontier, (f(child, goal, w), next(counter), child))
     return None
 
 
+def main():
+    init = Node(state=(0, 0), path_cost=0)
+    goal = Node(state=(9, 9))
+    w = 1.5
 
+    path = weighted_a_star(grid, init, goal, w)
+    if path:
+        print(path)
+    else:
+        print("No path found")
+
+if __name__ == "__main__":
+    main()
